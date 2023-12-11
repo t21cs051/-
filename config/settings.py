62237c53b2894bf2950115483e72b7686511d5cf
Takuma Mitsuzuka <t21cs051@yamanashi.ac.jp>
2023-12-12 00:49:52 +0900

    [clean] ディレクトリ構成の整理
    各アプリケーションをまとめるためのapps/,
    チュートリアルと同じtemplates/, static/ の作成
    
    bootstrapファイルの追加

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'TD1.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'TD1.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
