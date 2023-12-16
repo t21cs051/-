234003057cae75e117e2a17920ad90983a28f71a
Hayato Maruyama <t21cs050@yamanashi.ac.jp>
2023-12-16 19:18:42 +0900

    [add] htmlファイルの追加
    measurement内にhtmlファイルを追加し、ページが表示されるようにした。

--- apps/measurement/urls.py
+++ apps/measurement/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from .views import MeasurementListView, MeasurementAddView, MeasurementShowView
 
 app_name = "measurement"
 
 urlpatterns = [
+    
+    path('', MeasurementListView.as_view(), name='measurement'),
+    path('list/', MeasurementListView.as_view(), name='list'),
+    path('add/', MeasurementAddView.as_view(), name='add'),
+    path('show/', MeasurementShowView.as_view(), name='show'),
 ]
