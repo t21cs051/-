6f4f7d7ff55dd96272c356b48940de8e17ab0ffd
Takuma Mitsuzuka <t21cs051@yamanashi.ac.jp>
2023-12-21 11:48:52 +0900

    [add] chart.jsによるグラフ描画
    測定データをグラフに描画する機能の追加
    chart.jsをstaticに設置
    
    未実装の機能
    ・電源系統ごとの表示
    ・日付によるソート
    ・ラックごとのグラフ表示

--- apps/usage/urls.py
+++ apps/usage/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = "usage"
-
 urlpatterns = [
+    path('main/', views.UsageView.as_view(), name='main'),
 ]
--- apps/usage/views.py
+++ apps/usage/views.py
@@ -1,3 +1,24 @@
+import pandas as pd
+from django_pandas.io import read_frame
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic.base import TemplateView
+from apps.master.models import Rack
+from apps.measurement.models import CurrentMeasurement
 
-# Create your views here.
+class UsageView(TemplateView):
+    template_name = "usage/usage_main.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        measurement_queryset = CurrentMeasurement.objects.all()
+        df_line = read_frame(measurement_queryset,
+                            fieldnames=['measurement_date', 'current_value'])
+        df_line['measurement_date'] = pd.to_datetime(df_line['measurement_date'])
+        df_line['current_value'] = pd.to_numeric(df_line['current_value'])
+        measurement_date = df_line['measurement_date'].tolist()
+        current_value = df_line['current_value'].tolist()
+        context['measurement_date'] = measurement_date
+        context['current_value'] = current_value
+        return context
