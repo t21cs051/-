e78ae7c75d94486f4a58e69f66a247bb0ea38a37
Hayato Maruyama <t21cs050@yamanashi.ac.jp>
2023-12-22 16:03:01 +0900

    [clean]　homeのリファクタリング及びコメントの削除
    homeのview内を整理

--- apps/home/views.py
+++ apps/home/views.py
@@ -9,20 +9,19 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # 最新の測定データを取得
-        measurement_queryset = CurrentMeasurement.objects.all().order_by('-measurement_date')[:5]
+        # 電流測定データを取得
+        measurement_queryset = CurrentMeasurement.objects.all().order_by('-measurement_date')
 
-        # 最新の作業記録を取得
-        worklog_queryset = WorkLog.objects.all().order_by('-work_date')[:5]
+        # 作業記録データを取得
+        worklog_queryset = WorkLog.objects.all().order_by('-work_date')
 
-        # 測定データと作業記録を合わせて5つまで取得
+        # 測定データと作業記録を結合
         history_data = list(measurement_queryset) + list(worklog_queryset)
-        history_data = history_data[:5]  # 最初の5つだけを取得
+        history_data = history_data[:5]  # 最新5件
 
         # 日時でソート
         history_data.sort(key=lambda entry: entry.measurement_date if hasattr(entry, 'measurement_date') else entry.work_date, reverse=True)
 
-        # '内容' を追加
         context['history_data'] = [
             {'employee': entry.employee if hasattr(entry, 'employee') else 'N/A',
              'content': '電流' if hasattr(entry, 'measurement_date') else '作業',
