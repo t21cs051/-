6701176a9d39d5441f3fc2bedb570a4b0722d5a4
幸田 怜奈 <t21cs022@yamanashi.ac.jp>
2023-12-23 17:55:53 +0900

    ラック・UPS・電源系統のViewを改良

--- apps/master/urls.py
+++ apps/master/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import RackList,RackAddView, RackShowView,RackEditView,RackDeleteView
-from .views import UpsList, UpsAddView, UpsShowView, UpsEditView, UpsDeleteView
-from .views import PowerSystemList, PowerSystemAddView, PowerSystemShowView
-from .views import PowerSystemEditView, PowerSystemDeleteView, PowerSystemEditView, PowerSystemDeleteView, mainpage
+from .views import RackList,RackAddView,RackEditView,RackDeleteView
+from .views import UpsList, UpsAddView, UpsEditView, UpsDeleteView
+from .views import PowerSystemList, PowerSystemAddView, PowerSystemEditView, PowerSystemDeleteView, mainpage
 from .views import EmployeeList, EmployeeAddView, EmployeeEditView, EmployeeDeleteView
 
 app_name = "master"
@@ -10,19 +9,16 @@ urlpatterns = [
     path('main/', mainpage.as_view(), name='main'),
     path('rack/', RackList.as_view(), name='rack'),
     path('rack_add/', RackAddView.as_view(), name='rack_add'),
-    path('rack_show/', RackShowView.as_view(), name='rack_show'),
-    path('rack_edit/<int:id>/', RackEditView.as_view(), name='rack_edit'),
-    path('rack_delete/<int:id>/', RackDeleteView.as_view(), name='rack_delete'),
+    path('rack_edit/<int:pk>/', RackEditView.as_view(), name='rack_edit'),
+    path('rack_delete/<int:pk>/', RackDeleteView.as_view(), name='rack_delete'),
     path('ups/', UpsList.as_view(), name='ups'),
     path('ups_add/', UpsAddView.as_view(), name='ups_add'),
-    path('ups_show/', UpsShowView.as_view(), name='ups_show'),
-    path('ups_edit/<int:id>/', UpsEditView.as_view(), name='ups_edit'),
-    path('ups_delete/<int:id>/', UpsDeleteView.as_view(), name='ups_delete'),
+    path('ups_edit/<int:pk>/', UpsEditView.as_view(), name='ups_edit'),
+    path('ups_delete/<int:pk>/', UpsDeleteView.as_view(), name='ups_delete'),
     path('power_system/', PowerSystemList.as_view(), name='power_system'),
     path('power_system_add/', PowerSystemAddView.as_view(), name='power_system_add'),
-    path('power_system_show/', PowerSystemShowView.as_view(), name='power_system_show'),
-    path('power_system_edit/<int:id>/', PowerSystemEditView.as_view(), name='power_system_edit'),
-    path('power_system_delete/<int:id>/', PowerSystemDeleteView.as_view(), name='power_system_delete'),
+    path('power_system_edit/<int:pk>/', PowerSystemEditView.as_view(), name='power_system_edit'),
+    path('power_system_delete/<int:pk>/', PowerSystemDeleteView.as_view(), name='power_system_delete'),
     path('employee/', EmployeeList.as_view(), name='employee'),
     path('employee_add/', EmployeeAddView.as_view(), name='employee_add'),
     path('employee_edit/', EmployeeEditView.as_view(), name='employee_edit'),
--- apps/master/views.py
+++ apps/master/views.py
@@ -1,7 +1,7 @@
 from apps.accounts.models import CustomUser as Employee
 from django.views.generic import ListView
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.urls.base import reverse_lazy
@@ -18,7 +18,7 @@ class mainpage(TemplateView):
 
 
 #ラック番号表示画面
-class RackList(TemplateView):
+class RackList(ListView):
     model = Rack
     template_name = 'master/rack_list.html'
     
@@ -30,7 +30,7 @@ class RackList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['object_list'] = Rack.objects.all()
+        context['form_id'] = RackIdForm()
         return context
 
 class RackAddView(CreateView):
@@ -39,64 +39,31 @@ class RackAddView(CreateView):
     template_name = 'master/rack_add.html'
     success_url = reverse_lazy('master:rack')
 
-class RackShowView(TemplateView):
-    model =Rack
-    template_name = 'master/rack_show.html'
 
-    def post(self, request, *args, **kwargs):
-        rack_id = self.request.POST.get('rack_id')
-        rack = Rack.objects.get(pk=rack_id)
-        context = super().get_context_data(**kwargs)
-        context['form_id'] = RackIdForm()
-        context['rack'] = rack
-        return self.render_to_response(context)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form_id'] = RackIdForm()
-        return context
-
-class RackEditView(TemplateView):
+class RackEditView(UpdateView):
     model = Rack
-    fields = ('rack_number')
+    form_class = RackForm
     template_name = 'master/rack_edit.html'
-    success_url = 'rack/'
-
-    def post(self, request, *args, **kwargs):
-        rack_id = self.kwargs.get('id')
-        rack_number = self.request.POST.get('rack_number')
-        rack = get_object_or_404(Rack, pk=rack_id)
-        rack.rack_number = rack_number
-        rack.save()
-        return HttpResponseRedirect(reverse('master:rack'))
+    success_url = reverse_lazy('master:rack')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form_id'] = RackIdForm()
-        context['form'] = RackForm()
         return context
 
-class RackDeleteView(TemplateView):
+class RackDeleteView(DeleteView):
     model = Rack
     template_name = 'master/rack_delete.html'
+    success_url = reverse_lazy('master:rack')
         
-    def post(self, request, *args, **kwargs):
-        rack_id = self.kwargs.get('id')
-        rack = get_object_or_404(Rack, pk=rack_id)
-        rack.delete()
-        return HttpResponseRedirect(reverse('master:rack'))
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        rack_id = self.kwargs.get('id')
-        rack = get_object_or_404(Rack, pk=rack_id)
-        context['rack'] = rack
-        context['form'] = RackIdForm()
+        context['form_id'] = RackIdForm()
         return context
-        
-
+    
+    
 #UPS番号表示画面
-class UpsList(TemplateView):
+class UpsList(ListView):
     model = Ups 
     template_name = 'master/ups_list.html'
     
@@ -108,7 +75,7 @@ class UpsList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['object_list'] = Ups.objects.all()
+        context['form_id'] = UpsIdForm()
         return context
     
 class UpsAddView(CreateView):
@@ -117,64 +84,31 @@ class UpsAddView(CreateView):
     template_name = 'master/ups_add.html'
     success_url = reverse_lazy('master:ups')
 
-class UpsShowView(TemplateView):
-    model =Ups
-    template_name = 'master/ups_show.html'
-
-    def post(self, request, *args, **kwargs):
-        ups_id = self.request.POST.get('ups_id')
-        ups = Ups.objects.get(pk=ups_id)
-        context = super().get_context_data(**kwargs)
-        context['form_id'] = UpsIdForm()
-        context['ups'] = ups
-        return self.render_to_response(context)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form_id'] = UpsIdForm()
-        return context
-
-class UpsEditView(TemplateView):
+class UpsEditView(UpdateView):
     model = Ups
-    fields = ('ups_number')
+    form_class = UpsForm
     template_name = 'master/ups_edit.html'
-    success_url = 'ups/'
-
-    def post(self, request, *args, **kwargs):
-        ups_id = self.kwargs.get('id')
-        ups_number = self.request.POST.get('ups_number')
-        ups = get_object_or_404(Ups, pk=ups_id)
-        ups.ups_number = ups_number
-        ups.save()
-        return HttpResponseRedirect(reverse('master:ups'))
+    success_url = reverse_lazy('master:ups')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form_id'] = UpsIdForm()
-        context['form'] = UpsForm()
         return context
 
-class UpsDeleteView(TemplateView):
+class UpsDeleteView(DeleteView):
     model = Ups
     template_name = 'master/ups_delete.html'
-        
-    def post(self, request, *args, **kwargs):
-        ups_id = self.kwargs.get('id')
-        ups = get_object_or_404(Ups, pk=ups_id)
-        ups.delete()
-        return HttpResponseRedirect(reverse('master:ups'))
+    success_url = reverse_lazy('master:ups')
+    
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        ups_id = self.kwargs.get('id')
-        ups = get_object_or_404(Ups, pk=ups_id)
-        context['ups'] = ups
-        context['form'] = UpsIdForm()
+        context['form_id'] = UpsIdForm()
         return context
     
 
 #電源系統表示画面
-class PowerSystemList(TemplateView):
+class PowerSystemList(ListView):
     model = PowerSystem
     template_name = 'master/power_system_list.html'
     
@@ -186,7 +120,7 @@ class PowerSystemList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['object_list'] = PowerSystem.objects.all()
+        context['form_id'] = PowerSystemIdForm()
         return context
     
     
@@ -195,68 +129,33 @@ class PowerSystemAddView(CreateView):
     fields = ('power_system_number', 'max_current', 'supply_source', 'supply_rack')
     template_name = 'master/power_system_add.html'
     success_url = reverse_lazy('master:power_system')
-    
-class PowerSystemShowView(TemplateView):
+
+class PowerSystemEditView(UpdateView):
     model = PowerSystem
-    template_name = 'master/power_system_show.html'
-
-    def post(self, request, *args, **kwargs):
-        power_system_id = self.request.POST.get('power_system_id')
-        power_system = PowerSystem.objects.get(pk=power_system_id)
-        context = super().get_context_data(**kwargs)
-        context['form_id'] = PowerSystemIdForm()
-        context['power_system'] = power_system
-        return self.render_to_response(context)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form_id'] = PowerSystemIdForm()
-        return context
-
-class PowerSystemEditView(TemplateView):
-    model = PowerSystem
-    fields = ('power_system_number', 'max_current', 'supply_source', 'supply_rack')
+    form_class = PowerSystemForm
     template_name = 'master/power_system_edit.html'
-    success_url = 'power_system/'
-
-    def post(self, request, *args, **kwargs):
-        power_system_id = self.kwargs.get('id')
-        power_system = get_object_or_404(PowerSystem, pk=power_system_id)
-        form = PowerSystemForm(request.POST, instance=power_system)
-        form.save()
-        return HttpResponseRedirect(reverse('master:power_system'))
+    success_url = reverse_lazy('master:power_system')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        power_system_id = self.kwargs.get('id')
-        power_system = get_object_or_404(PowerSystem, pk=power_system_id)
-        form = PowerSystemForm(instance=power_system)
         context['form_id'] = PowerSystemIdForm()
-        context['form'] = form
         return context
 
-class PowerSystemDeleteView(TemplateView):
+class PowerSystemDeleteView(DeleteView):
     model = PowerSystem
     template_name = 'master/power_system_delete.html'
-        
-    def post(self, request, *args, **kwargs):
-        power_system_id = self.kwargs.get('id')
-        power_system = get_object_or_404(PowerSystem, pk=power_system_id)
-        power_system.delete()
-        return HttpResponseRedirect(reverse('master:power_system'))
-
+    success_url = reverse_lazy('master:power_system')
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        power_system_id = self.kwargs.get('id')
-        power_system = get_object_or_404(PowerSystem, pk=power_system_id)
-        context['power_system'] = power_system
-        context['form'] = PowerSystemIdForm()
+        context['power_system'] = self.get_object()
+        context['form_id'] = PowerSystemIdForm()
         return context
     
+#社員マスタ
 class EmployeeList(TemplateView):
     model = Employee
     template_name = 'master/employee_list.html'
-    
     
     def post(self, request, *args, **kwargs):
         employee_number = self.request.POST.get('employee_number')
