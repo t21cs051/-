8f82240d49d979450e6f1102a263fe56939dc9cd
Takuma Mitsuzuka <t21cs051@yamanashi.ac.jp>
2024-01-05 14:40:26 +0900

    [change] 測定値・作業記録の追加時の作業者のフォームを削除
    追加時の作業者は自動的にログインユーザーに指定するように変更
    データの修正時にはUpdateFormを新規に作成し、作業者の指定をできるように変更

--- apps/measurement/forms.py
+++ apps/measurement/forms.py
@@ -7,5 +7,9 @@ class MeasurementIdForm(forms.Form):
 class MeasurementForm(forms.ModelForm):
     class Meta:
         model = CurrentMeasurement
+        fields = ['measurement_date', 'current_value', 'power_system']
+
+class MeasurementUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CurrentMeasurement
         fields = ['measurement_date', 'current_value', 'power_system', 'employee']
-        
