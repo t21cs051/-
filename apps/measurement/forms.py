65ea75d7a9add35bfc60b4ef2f38a8737a7b16d6
Kubota Ryo <t21cs019@yamanashi.ac.jp>
2024-01-21 11:42:25 +0900

    [modify] 電流記録画面のフォームを整えた
    電流記録画面に適用しているブートストラップを修正
    電流記録画面のforms.pyにもブートストラップを適用した。

--- apps/measurement/forms.py
+++ apps/measurement/forms.py
@@ -8,6 +8,11 @@ class MeasurementForm(forms.ModelForm):
     class Meta:
         model = CurrentMeasurement
         fields = ['measurement_date', 'current_value', 'power_system']
+        widgets = {
+            'measurement_date': forms.TextInput(attrs={'class': 'form-control'}),
+            'current_value': forms.NumberInput(attrs={'class': 'form-control'}),
+            'power_system': forms.Select(attrs={'class': 'form-select'}),
+        }
 
 class MeasurementUpdateForm(forms.ModelForm):
     class Meta:
