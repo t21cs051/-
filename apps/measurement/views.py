35a602562af304bc97583c48eabfd2c9d675a0b0
Takuma Mitsuzuka <t21cs051@yamanashi.ac.jp>
2024-01-24 10:26:46 +0900

    [change] 修正画面のリストのソート
    データベースの日付が新しい順にソート

--- apps/measurement/views.py
+++ apps/measurement/views.py
@@ -3,25 +3,14 @@ from .models import CurrentMeasurement;
 from .forms import MeasurementIdForm, MeasurementForm, MeasurementUpdateForm
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView,  UpdateView, DeleteView
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.shortcuts import get_object_or_404
 from django.urls.base import reverse_lazy
 
 class MeasurementListView(ListView):
     model = CurrentMeasurement
     template_name = 'measurement/measurement_list.html'
-    
-    # TODO: このメソッドの役割の確認、必要？
-    def post(self, request, *args, **kwargs):
-        measurement_id = self.request.POST.get('measurement_id')
-        measurement_date = self.request.POST.get('measurement_date')
-        current_value = self.request.POST.get('current_value')
-        power_system = self.request.POST.get('power_system')
-        employee = self.request.POST.get('employee')
-        measurement = get_object_or_404(CurrentMeasurement, pk=measurement_id)
-        CurrentMeasurement.save()
-        return HttpResponseRedirect(reverse('measurement:list'))
+
+    def get_queryset(self):
+        return CurrentMeasurement.objects.order_by('-measurement_date')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- apps/worklog/views.py
+++ apps/worklog/views.py
@@ -9,13 +9,10 @@ from .forms import WorkLogIdForm, WorkLogForm, WorkLogUpdateForm
 
 class WorkLogListView(ListView):
     model = WorkLog
-    def post(self, request, *args, **kwargs):
-        worklog_id = self.request.POST.get('worklog_id')
-        worklog = get_object_or_404(WorkLog, pk=worklog_id)
-        worklog_status = self.request.POST.get('worklog_status')
-        # worklog.buy = worklog_status
-        worklog.save()
-        return HttpResponseRedirect(reverse('worklog:list'))
+    template_name = 'worklog/worklog_list.html'
+    
+    def get_queryset(self):
+        return WorkLog.objects.order_by('-work_date')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
